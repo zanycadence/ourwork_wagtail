a06f4be8cd36db5e9969c1c06678929688763e46
Kirill Dubovitskiy <kirill2003de@gmail.com>
2019-02-08 22:58:59 -0800

    basic data models (Member, ContractSpec) created

--- api/models.py
+++ api/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 # we need to map strings to integers for making actual predictions
 class Member(models.Model):
-    background_job = models.CharField(max_length=100)
-    background_location = models.CharField(max_length=100)
-    
+    ### background - match with the mentor on this
+    job = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    ### track/specialization
     track = models.CharField(max_length=100)
     # Entry | Project | Mentor
     track_progress = models.CharField(max_length=100)
-    
+
+    ### match with a job offer
+    skills = models.TextField()
+
+class ContractSpec(models.Model):
+    ### [0, 5]
+    # javascript,aws,nodejs
+    skills = models.TextField()
+
+    employer = models.CharField(max_length=100)
+
     
