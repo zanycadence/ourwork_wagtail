05d822fc20d70e705a7fe5991db49733d9d48b1b
Elijah Scheele <eli@zanycadence.com>
2019-02-09 14:14:52 -0800

    page models stubbed out (hopefully)

---
+++ ourwork/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from wagtail.core.models import Page
+
+
+class StudentPage(Page):
+    pass
+
+
+class MentorPage(Page):
+    pass
+
+
+class CommunityPage(Page):
+    pass
+
+
+class OnboardPage(Page):
+    pass
+
+
+class SelectMentorPage(Page):
+    pass
+
+
+class CoursePage(Page):
+    pass
+
+
+class JobPage(Page):
+    pass
+
+
+class MatchPage(Page):
+    pass
--- ourwork_wagtail/settings/base.py
+++ ourwork_wagtail/settings/base.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'api',
-
+    'ourwork',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
