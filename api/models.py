a2ebbccb10a0c8def62e1697d9367d188f4ddf49
InduManimaran <indu.manimaran@gmail.com>
2019-02-10 10:41:10 -0800

    added categories

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ml import * 
+from feature_dicts import *
 # helpers ml <-> model
 
 def flip_dict(d):
@@ -16,9 +17,44 @@ def array_to_string(arr):
     return s[:-1]
 
 job_to_feature = {
-    "Driver": 0
-    , "Burgers": 1
-    , "Cashier": 2
+     "Transportation": 1
+    ,"Retail": 2
+    ,"Manufacture":3
+    ,"Security":4
+    ,"Food": 5
+
+    }
+
+progress_to_feature ={
+    "Student":1
+    ,"Mentor":2
+    ,"Projects":3
+}
+
+track_to_feature = {
+    "Web Development":1
+    ,"Data Analytics":2
+    ,"Design":3
+}
+
+skills_dict={
+    "HTML":0
+    ,"CSS":1
+    ,"Javascript":2
+    ,"MongoDB":3 
+    ,"SQL":4 
+    ,"Python":5
+    , "R":6 
+    , "Tableau":7 
+    ,"MATLAB":8 
+    , "SAS":9
+    , "Visual Studio":10
+    , "Photoshop":11 
+    , "Illustrator":12
+    , "PHP":13
+    , "NodeJS":14 
+    , "Sketch":15
+    , "Angular":16
     }
 
 feature_to_job = flip_dict(job_to_feature)
---
+++ feature_dicts.py
@@ -0,0 +1,40 @@
+job_to_feature = {
+     "Transportation": 1
+    ,"Retail": 2
+    ,"Manufacture":3
+    ,"Security":4
+    ,"Food": 5
+
+    }
+
+progress_to_feature ={
+    "Student":1
+    ,"Mentor":2
+    ,"Projects":3
+}
+
+track_to_feature = {
+    "Web Development":1
+    ,"Data Analytics":2
+    ,"Design":3
+}
+
+skills_dict={
+    "HTML":0
+    ,"CSS":1
+    ,"Javascript":2
+    ,"MongoDB":3 
+    ,"SQL":4 
+    ,"Python":5
+    , "R":6 
+    , "Tableau":7 
+    ,"MATLAB":8 
+    , "SAS":9
+    , "Visual Studio":10
+    , "Photoshop":11 
+    , "Illustrator":12
+    , "PHP":13
+    , "NodeJS":14 
+    , "Sketch":15
+    , "Angular":16
+    }
